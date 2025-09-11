18b554c4daacf3335cef851c66496c3901e245cc
arbymungcal <arbycastro284@gmail.com>
2025-09-12 07:45:24 +0800

    update connection

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,11 +1,9 @@
+// app/api/images/route.ts in Site A
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { NextResponse } from "next/server";
-import { eq } from "drizzle-orm"; // only if you want filtering
 
-// GET all uploaded images
 export async function GET() {
   const allImages = await db.select().from(images);
-  
   return NextResponse.json(allImages);
 }
